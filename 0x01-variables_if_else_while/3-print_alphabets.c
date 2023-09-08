d22dc8e6651f367c3bc93e47bf33e4c6252cec05
hagar <hagarmohamed247@gmail.com>
2023-09-08 11:02:41 -0700

    3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,19 +9,18 @@
 */
 
 int main(void)
-
 {
 	char ch = 'a';
 	char CH = 'A';
 
-	/*prints a -z */
+	/*prints a -z*/
 	while (ch <= 'z')
 	{
 		putchar(ch);
 		ch++;
 	}
 
-	/*prints A - Z */
+	/*prints A - Z*/
 	while (CH <= 'Z')
 	{
 		putchar (CH);
