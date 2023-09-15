a91b9becb27eb36c6a971c163bd1bf87cbd68cdf
hagar <hagarmohamed247@gmail.com>
2023-09-15 08:18:29 -0700

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print numbers - print 0 - 9
+ * 		only using _putchar twice
+ *
+ * Return: Always 0 (success)
+ */
+
+void print_numbers(void)
+{
+	int n = 0;
+
+	while ( n >= 0 && n <= 9);
+	{
+		_putchar(num + 48);
+		num ++
+	}
+	_putchar ('\n');
+}
+
