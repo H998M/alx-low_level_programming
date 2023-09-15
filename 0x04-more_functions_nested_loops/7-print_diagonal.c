5603fd199ef00ce5e5901070a0a791712dfebc32
hagar <hagarmohamed247@gmail.com>
2023-09-15 12:57:33 -0700

    6

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -18,7 +18,7 @@ void print_diagonal(int n)
 	{
 		for (postn = 1; postn <= n; postn++)
 		{
-			for (space = 1; space <= postn; space++)
+			for (spaces = 1; spaces <= postn; spaces++)
 				_putchar(' ');
 			_putchar(92); /*is equal to '/' char*/
 		}	
