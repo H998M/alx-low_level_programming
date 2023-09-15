7d53d66673667c2fc1b0310802b13410476d300a
hagar <hagarmohamed247@gmail.com>
2023-09-15 08:49:09 -0700

    4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,14 +8,13 @@
 
 void print_most_numbers(void)
 {
-	int num = 0;
+	int num;
 
-	while (num >= 0 && num <= 9)
+	for (num = 0; num <= 9; num++)
 	{
 		if (num == 2 || num == 4)
 			continue;
 		_putchar(num + 48);
-		num++;
 	}
 	_putchar('\n');
 }
