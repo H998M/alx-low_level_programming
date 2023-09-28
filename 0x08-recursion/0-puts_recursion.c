98e05731c80302d5a7584c37f059b9537b8c2dd3
hagar <hagarmohamed247@gmail.com>
2023-09-28 11:50:16 -0700

    0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,7 +4,7 @@
  * _puts_recursion - prints a string followed by a new line
  * @s: pointer to the string
  * Return: void
-*/
+ */
 
 void _puts_recursion(char *s)
 {
@@ -14,7 +14,7 @@ void _puts_recursion(char *s)
 	}
 	else
 	{
-		_putcahr(*s);
+		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
 }
